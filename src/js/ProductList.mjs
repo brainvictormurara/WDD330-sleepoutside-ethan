c85181e34cd7a14a51d16dada55e6bb71411aa6c
Brain Victor Murara <bvmurara@gmail.com>
2026-09-12 22:49:56 +0200

    Fix product image paths

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -21,7 +21,7 @@ function productCardTemplate(product) {
   return `
     <li class="product-card">
       <a href="${pagePaths[product.Id]}">
-        <img src=".${product.Image}" alt="${product.NameWithoutBrand}">
+        <img src="${product.Image}" alt="${product.NameWithoutBrand}">
         <h3 class="card__brand">${product.Brand.Name}</h3>
         <h2 class="card__name">${product.NameWithoutBrand}</h2>
         <p class="product-card__price">
