58f983da4814cc639e2324ab5ee4ee2c271782c3
Brain Victor Murara <bvmurara@gmail.com>
2026-09-24 19:16:18 +0200

    Save previous Sleep Outside work

--- src/js/ProductDetails.js
+++ src/js/ProductDetails.js
@@ -1,7 +1,6 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 export default class ProductDetails {
-
   constructor(productId, dataSource) {
     this.productId = productId;
     this.product = {};
@@ -27,7 +26,6 @@ export default class ProductDetails {
     productDetailsTemplate(this.product);
   }
 }
-
 
 function productDetailsTemplate(product) {
   return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
